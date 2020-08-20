c5332cd845ed58a952901bcab9924f56e485feef
Edotnd <edotnd@gmail.com>
2020-08-20 22:30:02 +0900

    linear_search

--- Line_search.py
+++ Line_search.py
@@ -5,3 +5,13 @@ def bute_force_search(array, value):
         if element == value:
             return value, steps
     return None, steps
+
+def linear_search(array, value):
+    step = 0 
+    for element in array:
+        step += 1
+        if element == value:
+            return value, step
+        elif element > value:
+            break
+    return None, step
