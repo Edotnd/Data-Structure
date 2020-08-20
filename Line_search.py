58d8a1a0bcbe3103e1701b3914471fd64c14b8b2
Edotnd <edotnd@gmail.com>
2020-08-20 22:35:09 +0900

    정렬된 배열에서 Line Search

--- Line_search.py
+++ Line_search.py
@@ -15,3 +15,12 @@ def linear_search(array, value):
         elif element > value:
             break
     return None, step
+
+# 정렬된 배열
+data = [3, 17, 75, 80, 202]
+
+linear_search_r, linear_search_steps = linear_search(data, 22)
+print(linear_search_r, linear_search_steps)
+
+bute_force_search_r, bute_force_search_steps = bute_force_search(data, 22)
+print(bute_force_search_r, bute_force_search_steps)
