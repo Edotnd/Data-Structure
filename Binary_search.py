7dde481c8ca71576ea4d75c31e8de5834c765ac3
Edotnd <edotnd@gmail.com>
2020-08-20 23:56:21 +0900

    정렬된 배열에서 Binary search

--- Binary_search.py
+++ Binary_search.py
@@ -15,9 +15,11 @@ def binary_search(array, value):
             lower_bound = midpoint + 1
         elif value == value_at_midpoint:
             return midpoint, steps
-            
+
     return None, steps
 
+# 정렬된 배열에서 이진검색
 data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 binary_search_r, binary_search_steps = binary_search(data, 5)
 print(binary_search_r, binary_search_steps)
