09a9386d31f6f8bdcbaa6d5f4dd0e93f8ce3b942
Edotnd <edotnd@gmail.com>
2020-08-22 23:27:20 +0900

    Graph Algorithm

---
+++ Graph.py
@@ -0,0 +1,18 @@
+'''
+     John - Summer - Mike       Tom
+       |      |       |          |
+       + -- Justin -- +        Jerry
+              |
+             May
+              |
+             Kim
+'''
+fr_info = {
+    "Summer": ['John', 'Justin', 'Mike'],
+    "John": ['Summer', 'Justin'],
+    "Justin": ['John', 'Summer', 'Mike', 'May'],
+    "Mike": ['Summer', 'Justin'],
+    "Kim": ['May'],
+    "Tom": ['Jerry'],
+    "Jerry": ['Tom']
+}
